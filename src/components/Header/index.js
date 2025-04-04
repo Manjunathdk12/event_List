fd45e408edaf2d929d118e8e7e3247c6b1d18d70
manjunath <manjunathhatti012@gmail.com>
2025-04-04 19:53:42 +0530

    added new file

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,44 +1,34 @@
 import { Link, useLocation } from 'react-router-dom'
+import { useState } from 'react'
+import { Menu, X } from 'lucide-react'
 import './index.css'
 
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
   const location = useLocation()
+
+  const toggleMenu = () => setIsMenuOpen(!isMenuOpen)
 
   return (
     <nav className="navbar">
-      <h2 className='logo'>CommunionHub</h2>
-      <ul className='navPage'>
+      <Link to="/" className="logo" >CommunionHub</Link>
+
+      <div className="menu-icon" onClick={toggleMenu}>
+        {isMenuOpen ? <X size={28} color="white" /> : <Menu size={28} color="white" />}
+      </div>
+
+      <ul className={`navPage ${isMenuOpen ? 'open' : ''}`}>
         <li>
-          <Link
-            to="/"
-            className={`link ${location.pathname === '/' ? 'active-link' : ''}`}
-          >
-            Home
-          </Link>
+          <Link to="/" className={`link ${location.pathname === '/' ? 'active' : ''}`} onClick={toggleMenu}>Home</Link>
         </li>
         <li>
-          <Link
-            to="/events"
-            className={`link ${location.pathname === '/events' ? 'active-link' : ''}`}
-          >
-            Events
-          </Link>
+          <Link to="/events" className={`link ${location.pathname === '/events' ? 'active' : ''}`} onClick={toggleMenu}>Events</Link>
         </li>
         <li>
-          <Link
-            to="/about"
-            className={`link ${location.pathname === '/about' ? 'active-link' : ''}`}
-          >
-            About
-          </Link>
+          <Link to="/about" className={`link ${location.pathname === '/about' ? 'active' : ''}`} onClick={toggleMenu}>About</Link>
         </li>
         <li>
-          <Link
-            to="/login"
-            className={`link ${location.pathname === '/login' ? 'active-link' : ''}`}
-          >
-            Login
-          </Link>
+          <Link to="/login" className={`link ${location.pathname === '/login' ? 'active' : ''}`} onClick={toggleMenu}>Login</Link>
         </li>
       </ul>
     </nav>
--- src/components/NotFound/index.js
+++ src/components/NotFound/index.js
@@ -1,10 +1,9 @@
-// src/components/NotFound/index.js
 import './index.css'
 
 const NotFound = () => (
-  <div className="not-found-container">
-    <h1>404</h1>
-    <p>Oops! The page you're looking for doesn't exist.</p>
+  <div className="not-found">
+    <h1>404 - Page Not Found</h1>
+    <p>The page you are looking for doesn’t exist.</p>
   </div>
 )
 
